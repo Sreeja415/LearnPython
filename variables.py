3c59e5da07c7a8e4ff3f6f485241e6888dc53dc3
Sreeja415 <11sreejadadige@gmail.com>
2025-03-04 20:17:55 -0800

    sreeja detail program

---
+++ variables.py
@@ -0,0 +1,10 @@
+#Variables are containers for storing data values.
+name = "sreeja" 
+age =24
+place = "bothell"
+zipcode = 98012
+print ("Name : "  + name)
+print ("Age : "  + str( age))
+print("Place : "  + place)
+print("Zip Code : "  + str(zipcode))
+
