63116d42596d4c216dfb1ce3151a27d954ba8a60
Sreeja415 <11sreejadadige@gmail.com>
2025-03-05 16:36:08 -0800

    changes in variable input

---
+++ variablesinput.py
@@ -0,0 +1,12 @@
+# taking input 
+name = input("Enter your name : ")
+age = int(input("Enter your age : "))
+place = input("Enter your place : ")
+zipcode = int(input("Enter your zipcode : "))
+print("Your Details are bellow : ")
+print("Name : " + name ) 
+print("Age : " ,age)
+print("Place : " + place ) 
+print("Zipcode : " , zipcode) 
+
+
