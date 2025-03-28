cff725007e38f0497f7faf23a5749ba98bddebdf
Sreeja415 <11sreejadadige@gmail.com>
2025-03-28 16:09:52 -0700

    guessing program

--- guessingNumbers.py
+++ guessingNumbers.py
@@ -4,7 +4,7 @@ secret_number = 9 # Set the secret_number variable here (between 1 and 10)
 guess = 0
  #Initialize the guess variable here with a value of 0
 
-while guess != secret_number: # Add the while loop condition here
+while guess not secret_number: # Add the while loop condition here
 	guess = int(input("guess the secret_number:"))
 	print("Guessing:",guess)
 
