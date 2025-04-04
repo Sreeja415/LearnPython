ba1b1687cb17f5052bf0822ba668533c747a4be6
Sreeja415 <11sreejadadige@gmail.com>
2025-04-03 17:33:30 -0700

    odd even numbers

---
+++ oddevennumbers.py
@@ -0,0 +1,9 @@
+numbers = (100,200,300,400)
+list = [100,200,300,400]
+list.append(700)
+for i in list :
+    if i%2==0 :
+        print(i,"is even number")
+    else :
+        print(i,"is odd number")
+        
---
+++ oddevenusingindex.py
@@ -0,0 +1,6 @@
+numbers = [3, 9, 1, 10, 5, 2, 8]
+for index in range (0,len(numbers)):
+    if numbers[index] % 2 ==0 :
+        print(numbers[index],"is even")
+    else :
+        print(numbers[index],"is odd")
