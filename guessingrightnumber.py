e0227f944097cc5bb47949b2621084b162d8211f
Sreeja415 <11sreejadadige@gmail.com>
2025-04-09 12:30:54 -0700

    adding guessing program

---
+++ guessingrightnumber.py
@@ -0,0 +1,18 @@
+numbers = [1,3,5,7,9,0]
+value=False
+guess = int(input("Guess a number :"))
+for number in numbers :   
+   if number==guess :
+        value = True
+if value == True :
+    print ("guessed right")
+else:
+    print ("guessed wrong")
+       
+        
+ 
+
+   
+
+        
+
