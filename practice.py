0eccdb785d8c90e218266e7aa02c55a8ea41bdbb
Sreeja415 <11sreejadadige@gmail.com>
2025-05-30 15:05:46 -0700

    ading new program

---
+++ practice.py
@@ -0,0 +1,49 @@
+temperatures = [1, 2, 3, 4]
+highest_temperature = max(temperatures)
+lowest_temperature = min(temperatures)
+
+print("Highest Temperature:", highest_temperature)
+print("Lowest Temperature:", lowest_temperature)
+
+
+temperatures = [1, 2, 3, 4]
+average_temperature = sum(temperatures) / len(temperatures)
+print("Average Temperature:", average_temperature)
+
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero!"
+
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Enter choice (1/2/3/4): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == '1':
+    print("Result:", add(num1, num2))
+elif choice == '2':
+    print("Result:", subtract(num1, num2))
+elif choice == '3':
+    print("Result:", multiply(num1, num2))
+elif choice == '4':
+    print("Result:", divide(num1, num2))
+else:
+    print("Invalid input")
--- variablesinput.py
+++ variablesinput.py
@@ -4,9 +4,9 @@ age = int(input("Enter your age : "))
 place = input("Enter your place : ")
 zipcode = int(input("Enter your zipcode : "))
 print("Your Details are bellow : ")
-print("Name : " + name ) 
+print("Name : " ,name ) 
 print("Age : " ,age)
-print("Place : " + place ) 
+print("Place : " ,place ) 
 print("Zipcode : " , zipcode) 
 
 
