3bec98d7e1136be0256e70b883f7189f599eb8a4
Sreeja415 <11sreejadadige@gmail.com>
2025-06-04 16:11:56 -0700

    adding twosum leetcode

--- twosumleetcode.py
+++ twosumleetcode.py
@@ -7,8 +7,8 @@ class Solution(object):
         """
         num_map = {}  
 
-        for i, num in enumerate(nums):
+        for j, num in enumerate(nums):
             complement = target - num
             if complement in num_map:
-                return [num_map[complement], i]
-            num_map[num] = i
+                return [num_map[complement], j]
+            num_map[num] = j
